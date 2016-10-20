4227ad73ad9b744bbd234a0052279fedd0acd251
Josep Pascual Badia <joseppascualbadia@gmail.com>
2016-10-21 01:25:28 +0200

    Commented form

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -10,13 +10,24 @@ if(empty($_POST['name'])  		||
 	return false;
    }
 	
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
+// Create the email and send the message
+$to = 'joseppascualbadia@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+
 // Replace sender@example.com with your "From" address. 
 // This address must be verified with Amazon SES.
-define('SENDER', 'sender@example.com');        
+define('SENDER', 'joseppascualbadia@gmail.com');        
 
 // Replace recipient@example.com with a "To" address. If your account 
 // is still in the sandbox, this address must be verified.
-define('RECIPIENT', 'recipient@example.com');  
+define('RECIPIENT', 'joseppascualbadia@gmail.com');  
                                                       
 // Replace smtp_username with your Amazon SES SMTP user name.
 define('USERNAME','smtp_username');  
@@ -33,8 +44,8 @@ define('HOST', 'email-smtp.us-west-2.amazonaws.com');
 define('PORT', '587');     
 
 // Other message information                                               
-define('SUBJECT','Amazon SES test (SMTP interface accessed using PHP)');
-define('BODY','This email was sent through the Amazon SES SMTP interface by using PHP.');
+define('SUBJECT','$email_subject');
+define('BODY','$email_body');
 
 require_once 'Mail.php';
 
@@ -58,22 +69,8 @@ $mail = Mail::factory('smtp', $smtpParams);
 $result = $mail->send(RECIPIENT, $headers, BODY);
 
 if (PEAR::isError($result)) {
-  echo("Email not sent. " .$result->getMessage() ."\n");
+  return false;
 } else {
-  echo("Email sent!"."\n");
-}
-
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-	
-// Create the email and send the message
-$to = 'joseppascualbadia@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+  return true;
+}	
 ?>
